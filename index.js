eb4d6e15a5a8abbd1a3ecbb23c51f5fd7e78abc2
Michael <michael.liza.2003@gmail.com>
2026-04-27 15:50:27 -0500

    api

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('API CRUD funcionando ');
+  });
 let productos = [];
 
 // CREATE
